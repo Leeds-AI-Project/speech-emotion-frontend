3614db984f7d836ca92543c00ac1351a3c625c79
Vero060830 <sc21x2l@leeds.ac.uk>
2024-06-17 18:20:24 +0800

    My Initialize

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+        hhh
       <HelloWorld msg="You did it!" />
 
       <nav>
