6f77ebb099159f9a648abe772af30c527de876d9
Vero060830 <sc21x2l@leeds.ac.uk>
2024-06-20 23:34:59 +0800

    Finish upload api

--- src/components/MusicUploader.vue
+++ src/components/MusicUploader.vue
@@ -1,71 +1,62 @@
 <template>
-    <div class="header">
-        <img src="../assets/imgs/logo.png" style="height:40px;">
-    </div>
+  <div class="header">
+    <img src="../assets/imgs/logo.png" style="height: 40px" />
+  </div>
 
-    <van-swipe class="my-swipe" indicator-color="grey">
-        <van-swipe-item>
-            <VoiceRecorder></VoiceRecorder>
+  <van-swipe class="my-swipe" indicator-color="grey">
+    <van-swipe-item>
+      <VoiceRecorder></VoiceRecorder>
+    </van-swipe-item>
+    <van-swipe-item>
+      <UpLoader></UpLoader>
+    </van-swipe-item>
+  </van-swipe>
 
-        </van-swipe-item>
-        <van-swipe-item>
-            <UpLoader></UpLoader>
-        </van-swipe-item>
-    </van-swipe>
-
-
-    <van-tabbar v-model="active">
-        <van-tabbar-item icon="audio">音频分析</van-tabbar-item>
-        <van-tabbar-item icon="manager">我的</van-tabbar-item>
-    </van-tabbar>
-
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="audio">音频分析</van-tabbar-item>
+    <van-tabbar-item icon="manager">我的</van-tabbar-item>
+  </van-tabbar>
 </template>
 
 <script>
-import UpLoader from "@/components/UpLoader.vue";
-import VoiceRecorder from "@/components/VoiceRecorder.vue";
-
+import UpLoader from '@/components/UpLoader.vue'
+import VoiceRecorder from '@/components/VoiceRecorder.vue'
+import axios from 'axios'
 export default {
-    name: "index",
-    components: {VoiceRecorder, UpLoader}
+  name: 'index',
+  components: { VoiceRecorder, UpLoader }
 }
 </script>
 
 <style scoped>
 body {
-    padding: 0px;
+  padding: 0px;
 }
 
 .header {
-    text-align: center;
-    margin: 0;
-    padding: 0;
-    height: 50px;
-    background: #3377dd;
-    line-height: 50px;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  height: 50px;
+  background: #3377dd;
+  line-height: 50px;
 }
 
-
-:deep(.van-swipe__indicator )
-{
-    --van-swipe-indicator-size: 15px;
-    opacity:1;
-    --van-swipe-indicator-active-opacity:0.2;
+:deep(.van-swipe__indicator) {
+  --van-swipe-indicator-size: 15px;
+  opacity: 1;
+  --van-swipe-indicator-active-opacity: 0.2;
 }
 .my-swipe .van-swipe-item {
-
-
-    color: #fff;
-    font-size: 20px;
-    line-height: 150px;
-    text-align: center;
-
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
 }
-.my-swipe .van-swipe-item[data-v-cc12edcf]{
-    line-height: 0px;
+.my-swipe .van-swipe-item[data-v-cc12edcf] {
+  line-height: 0px;
 }
-.my-swipe{
-    height:650px;
-
+.my-swipe {
+  height: 600px;
 }
 </style>
